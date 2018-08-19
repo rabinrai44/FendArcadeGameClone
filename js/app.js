fff6e7f6a032f846d6ad73f893ac0a92c92b0506
Rabin Rai <rabinrai44@gmail.com>
2018-08-19 12:31:42 -0400

    Update hero constructor, added moveable hero within the boundaries

--- js/app.js
+++ js/app.js
@@ -75,15 +75,67 @@ document.addEventListener("keyup", function(e) {
 
 class Hero {
   constructor() {
-    this.x = 0;
-    this.y = 0;
     this.sprite = "images/char-boy.png";
+    this.step = 101;
+    this.jump = 83;
+    this.startX = this.step * 2;
+    this.startY = this.jump * 5 - 20;
+    this.x = this.startX;
+    this.y = this.startY;
   }
 
   // Draw hero sprite on current x and y coord position
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
+
+  /**
+   * Update hero's x and y property according to input
+   *
+   * @param {string} input - Direction to travel
+   *
+   */
+
+  handleInput(input) {
+    switch (input) {
+      case "left":
+        if (this.x > 0) {
+          this.x -= this.step;
+        }
+        break;
+
+      case "up":
+        if (this.y > 0) {
+          this.y -= this.jump;
+        }
+        break;
+
+      case "right":
+        if (this.x < this.step * 4) {
+          this.x += this.step;
+        }
+        break;
+
+      case "down":
+        if (this.y < this.jump * 4) {
+          this.y += this.jump;
+        }
+        break;
+    }
+  }
 }
 
 const player = new Hero();
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener("keyup", function(e) {
+  var allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down"
+  };
+
+  player.handleInput(allowedKeys[e.keyCode]);
+});
